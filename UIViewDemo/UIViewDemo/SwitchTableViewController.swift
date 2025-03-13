c85c583a20fe98f60e0a3cf83af659428511a096
aerohero <aerohero@icloud.com>
2025-03-13 15:07:41 +0900

    UITableView + Switch + Button

--- UIViewDemo/UIViewDemo/SwitchTableViewController.swift
+++ UIViewDemo/UIViewDemo/SwitchTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FormTableViewController.swift
+//  SwitchTableViewController.swift
 //  UIViewDemo
 //
 //  Created by Sean on 3/13/25.
@@ -7,21 +7,24 @@
 
 import UIKit
 
-class FormTableViewController: UITableViewController {
+class SwitchTableViewController: UITableViewController {
 
+    var flag = false
+    let toggle = UISwitch()
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tableView.separatorColor = .black
         
-//        toggle.addTarget(self, action: #selector(toggleChanged), for: .valueChanged)
-//        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-//        
-//        var config = UIButton.Configuration.filled()
-//        config.baseBackgroundColor = .systemBlue
-//        config.title = "Button"
-//        button.isEnabled = flag
-//        button.configuration = config
-//        button.sizeToFit()
+        toggle.addTarget(self, action: #selector(toggleChanged), for: .valueChanged)
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        
+        var config = UIButton.Configuration.filled()
+        config.baseBackgroundColor = .systemBlue
+        config.title = "Button"
+        button.isEnabled = flag
+        button.configuration = config
+        button.sizeToFit()
         
         tableView.headerView(forSection: 0)?.backgroundColor = .systemRed
     }
@@ -29,19 +32,12 @@ class FormTableViewController: UITableViewController {
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 4
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        switch section {
-        case 0:
-            return 3
-        case 3:
-            return 4
-        default:
             return 2
-        }
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
@@ -56,12 +52,12 @@ class FormTableViewController: UITableViewController {
         // 테이블 뷰 셀을 재사용 하거나 새로 생성
         let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
         
-//        if indexPath.row == 0 {
-//            toggle.isOn = flag
-//            cell.accessoryView = toggle
-//        } else {
-//            cell.accessoryView = button
-//        }
+        if indexPath.row == 0 {
+            toggle.isOn = flag
+            cell.accessoryView = toggle
+        } else {
+            cell.accessoryView = button
+        }
         
         // Configure the cell...
         var config = UIListContentConfiguration.subtitleCell()
@@ -74,27 +70,20 @@ class FormTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        switch indexPath.section {
-        case 0:
             return 50
-        case 1:
-            return 100
-        default:
-            return 150
-        }
     }
     
-//    // MARK: - objc selectors
-//    @objc func toggleChanged(_ sender: UISwitch) {
-//        flag = sender.isOn
-//        button.isEnabled = flag
-//    }
-//    
-//    @objc func buttonTapped(_ sender: UIButton) {
-//        print("Button tapped")
-//    }
+    // MARK: - objc selectors
+    @objc func toggleChanged(_ sender: UISwitch) {
+        flag = sender.isOn
+        button.isEnabled = flag
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        print("Button tapped")
+    }
 }
 
 #Preview {
-  FormTableViewController(style: .insetGrouped)
+  SwitchTableViewController(style: .insetGrouped)
 }
