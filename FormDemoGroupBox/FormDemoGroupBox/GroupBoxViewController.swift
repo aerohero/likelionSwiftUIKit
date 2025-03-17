797da84fcab2b83b98767d9bb6d556e9c203dbeb
aerohero <aerohero@icloud.com>
2025-03-17 11:26:43 +0900

    키보드 알림으로 코드 변경 버전

---
+++ FormDemoGroupBox/FormDemoGroupBox/GroupBoxViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FormDemoGroupBox
+//
+//  Created by Sean on 3/17/25.
+//
+
+import UIKit
+
+class GroupBoxViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
