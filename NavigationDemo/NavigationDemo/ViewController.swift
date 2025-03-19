4ad49694efa98460c3b4d2ad78e823359d9a2a3f
aerohero <aerohero@icloud.com>
2025-03-19 22:08:58 +0900

    네비게이션 바에 여러 바버튼 추가

--- NavigationDemo/NavigationDemo/ViewController.swift
+++ NavigationDemo/NavigationDemo/ViewController.swift
@@ -13,20 +13,29 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         self.title = "네비게이션 타이틀"
         
-        // iOS 15 이상에서는 UINavigationBarAppearance 사용
-        let appearance = UINavigationBarAppearance()
-        // 투명한 배경 설정
-        appearance.configureWithOpaqueBackground()
-        // 배경색 설정
-        appearance.backgroundColor = .systemBlue
-        // 타이틀 색상
-        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        self.navigationController?.navigationBar.barStyle = .default
+        self.navigationController?.navigationBar.tintColor = .white
+        self.navigationController?.navigationBar.isTranslucent = true
+        self.navigationController?.navigationBar.barTintColor = .systemBlue
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        // 네비게이션 바의 큰 타이틀 설정
+        //    self.navigationController?.navigationBar.prefersLargeTitles = true
+        //    self.navigationController?.navigationBar.backgroundColor = .systemBlue
         
-        // 네비게이션 바에 적용 (평상시)
-        self.navigationController?.navigationBar.standardAppearance = appearance
-        // 스와이프로 스크롤할 때
-        self.navigationController?.navigationBar.scrollEdgeAppearance = appearance
-        self.navigationController?.navigationBar.tintColor = .white // 아이템 색상
+        
+        
+        
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let VC = SecondViewController()
+        
+        self.navigationController?.pushViewController(VC, animated: true)
     }
 }
-
+#Preview {
+    UINavigationController(rootViewController: ViewController())
+}
