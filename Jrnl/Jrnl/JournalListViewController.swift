fe0c66a782d25c4ca02147d335c7feda92775cd3
aerohero <aerohero@icloud.com>
2025-03-25 16:21:08 +0900

    JNRL App Diary Detail Draft & Add Map

--- Jrnl/Jrnl/JournalListViewController.swift
+++ Jrnl/Jrnl/JournalListViewController.swift
@@ -23,7 +23,7 @@ class JournalListViewController: UIViewController {
 
 extension JournalListViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 0
+    return 10
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
