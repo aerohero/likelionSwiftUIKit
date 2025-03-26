4a135172dc22e8fad2f9b963c00eec58a61338fc
aerohero <aerohero@icloud.com>
2025-03-26 11:29:59 +0900

    journalCell을 스토리보드에 연결

--- Jrnl/Jrnl/JournalListViewController.swift
+++ Jrnl/Jrnl/JournalListViewController.swift
@@ -11,23 +11,33 @@ class JournalListViewController: UIViewController {
   
   @IBOutlet weak var tableView: UITableView!
   
+  var sampleJournalEntryData = SampleJournalEntryData()
   
   override func viewDidLoad() {
     super.viewDidLoad()
+    // 샘플 데이터 생성
+    sampleJournalEntryData.createSampleJournalEntryData()
   }
   
   @IBAction func unwindNewEntryCancel(segue: UIStoryboardSegue) {
-    
+    print("unwindNewEntryCancel")
   }
 }
 
 extension JournalListViewController: UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return sampleJournalEntryData.journalEntries.count
   }
   
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    return tableView.dequeueReusableCell(withIdentifier: "JournalCell", for: indexPath)
+    let journalCell = tableView.dequeueReusableCell(withIdentifier: "journalCell", for: indexPath) as! JournalListTableViewCell
+    
+    let journalEntry = sampleJournalEntryData.journalEntries[indexPath.row]
+    journalCell.dateLabel.text = journalEntry.date.formatted(.dateTime.month().day().year())
+    journalCell.titleLabel.text = journalEntry.entryTitle
+    journalCell.photoImageView.image = journalEntry.photo
+    
+    return journalCell
   }
 }
 
