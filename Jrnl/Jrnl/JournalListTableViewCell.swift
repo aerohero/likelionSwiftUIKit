816a23478dbfd023248d240f4d5433ed100cf1fc
aerohero <aerohero@icloud.com>
2025-03-26 10:52:25 +0900

    JournalEntry file and JournalListTableViewCell file 생성

---
+++ Jrnl/Jrnl/JournalListTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  JournalListTableViewCell.swift
+//  Jrnl
+//
+//  Created by Sean on 3/26/25.
+//
+
+import UIKit
+
+class JournalListTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var photoImageView: UIImageView!
+  @IBOutlet weak var dateLabel: UILabel!
+  @IBOutlet weak var titleLabel: UILabel!
+  
+}
