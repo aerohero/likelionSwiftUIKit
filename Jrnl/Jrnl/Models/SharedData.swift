a5c0891cfcd145d7dfe1d5e8ab99e66e7b0d88f2
aerohero <aerohero@icloud.com>
2025-03-29 19:29:05 +0900

    SharedData 모델 추가 및 저널 항목 관리 기능 구현

---
+++ Jrnl/Jrnl/Models/SharedData.swift
@@ -0,0 +1,45 @@
+//
+//  SharedData.swift
+//  Jrnl
+//
+//  Created by Sean on 3/29/25.
+//
+
+import Foundation
+
+class SharedData {
+  // MARK: - Properties
+  static let shared = SharedData()
+  private var journalEntries: [JournalEntry]
+
+  // MARK: - Initializer
+  private init() {
+    journalEntries = []
+  }
+
+  // MARK: access methods
+
+  // 데이터 개수 반환
+  func numberOfJournalEntries() -> Int {
+    return journalEntries.count
+  }
+
+  // 전체 데이터 반환
+  func getAllJournalEntries() -> [JournalEntry] {
+    return journalEntries
+  }
+
+  func getJournalEntry(at index: Int) -> JournalEntry {
+    return journalEntries[index]
+  }
+
+  // 데이터 추가
+  func addJournalEntry(_ entry: JournalEntry) {
+    journalEntries.append(entry)
+  }
+
+  // 데이터 삭제
+  func removeJournalEntry(at index: Int) {
+    journalEntries.remove(at: index)
+  }
+}
